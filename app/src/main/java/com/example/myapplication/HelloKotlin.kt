7aaf52baf27934e35b69112dacbdb82cc736a961
Juan R <juan.restrepo01@globant.com>
2025-03-27 11:47:38 -0500

    For pinto

---
+++ app/src/main/java/com/example/myapplication/HelloKotlin.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+fun main() {
+    println("Hola Kotlin")
+
+    //Variables
+
+    var myString="Esto es una cadena de texto"
+    myString="EL valor ha cambiado"
+    println(myString)
+
+    var myInt = 5
+    println(myInt)
+
+    var myDouble= 2.5
+    println(myDouble)
+
+    //Constantes
+
+    val myConst="Valor constante"
+    println(myConst)
+}
