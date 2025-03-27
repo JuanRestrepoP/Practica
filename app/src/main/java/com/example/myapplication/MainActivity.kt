f4be3dc1b94ff022d13592d3be16d0bbd45d049a
Juan R <juan.restrepo01@globant.com>
2025-03-27 10:11:55 -0500

    Hola

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "World",
+                        name = "Juan",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
